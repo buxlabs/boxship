25862e365ca15c7bcfcc819eead68b77d79a0c0c
Emil Ajdyna <emil@ajdyna.com>
2017-11-28 09:45:15 +0100

    Fix script

--- src/strategies/MyDevilNet.js
+++ src/strategies/MyDevilNet.js
@@ -5,9 +5,9 @@ class MyDevilNetStrategy extends Strategy {
     this.exec(`ssh -l ${this.username} ${this.host} 'rm -rf ${this.location}/public/*'`, { silent: this.verbose });
   }
   copy() {
-    const destination = `${this.username}@${this.host}:${this.location}/public`
+    const destination = `${this.location}/public`
     this.exec(`ssh -l ${this.username} ${this.host} 'mkdir -p ${destination}'`, { silent: this.verbose })
-    this.exec(`scp -r build/* ${destination}`, { silent: this.verbose });
+    this.exec(`scp -r build/* ${this.username}@${this.host}:${destination}`, { silent: this.verbose });
   }
   restart() {
     this.exec(`ssh -l ${this.username} ${this.host} 'devil www restart ${this.domain}'`, { silent: this.verbose });
