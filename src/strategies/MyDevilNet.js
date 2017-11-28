c0ceb3288c8c1a981ded4620538999909c42852a
Emil Ajdyna <emil@ajdyna.com>
2017-11-28 09:40:26 +0100

    Use verbose

--- src/strategies/MyDevilNet.js
+++ src/strategies/MyDevilNet.js
@@ -2,13 +2,15 @@ const Strategy = require('../Strategy')
 
 class MyDevilNetStrategy extends Strategy {
   clean() {
-    this.exec(`ssh -l ${this.username} ${this.host} 'rm -rf ${this.location}/public/*'`, { silent: true });
+    this.exec(`ssh -l ${this.username} ${this.host} 'rm -rf ${this.location}/public/*'`, { silent: this.verbose });
   }
   copy() {
-    this.exec(`scp -r build/* ${this.username}@${this.host}:${this.location}/public`, { silent: true });
+    const destination = `${this.username}@${this.host}:${this.location}/public`
+    this.exec(`ssh -l ${this.username} ${this.host} 'mkdir -p ${destination}'`, { silent: this.verbose })
+    this.exec(`scp -r build/* ${destination}`, { silent: this.verbose });
   }
   restart() {
-    this.exec(`ssh -l ${this.username} ${this.host} 'devil www restart ${this.domain}'`, { silent: true });
+    this.exec(`ssh -l ${this.username} ${this.host} 'devil www restart ${this.domain}'`, { silent: this.verbose });
   }
 }
 
